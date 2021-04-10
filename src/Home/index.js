4553562d69941b5a24420b7155210c215ba3c7ea
linedotstar <linedotstar@gmail.com>
2021-04-10 14:16:52 -0700

    add a placeholder to the thanks form

--- src/Home/index.js
+++ src/Home/index.js
@@ -3,30 +3,47 @@ import { generatePath, Link } from 'react-router-dom';
 import './Home.css';
 
 function Home(props) {
-  const [recipient, setRecipient] = useState('someone');
-  const thanksPath = generatePath('/thanks/:recipient', { recipient });
-  const thanksUrl = `http://coolthx.com${thanksPath}`;
+  const [recipient, setRecipient] = useState('');
 
   return (
     <>
       <h1>Thank Someone</h1>
-      <form>
-        <label>Who do you want to thank?</label>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label for='recipient-input'>
+          Who do you want to thank?
+        </label>
         <input
+          id='recipient-input'
           type='text'
           value={recipient}
           onChange={(e) => setRecipient(e.target.value)}
+          placeholder='someone'
         />
       </form>
 
-      <p>
-        {recipient.length > 0 && (
-          <Link to={thanksPath}>
-            {thanksUrl}
-          </Link>
-        )}
-      </p>
+      <ThanksLink recipient={recipient} />
     </>
+  );
+}
+
+function ThanksLink(props) {
+  const { recipient } = props;
+
+  if (!recipient || recipient.length <= 0) {
+    return null;
+  }
+
+  const thanksPath = generatePath('/thanks/:recipient', { recipient });
+  const thanksUrl = `http://coolthx.com${thanksPath}`;
+
+  return (
+    <p>
+      {recipient.length > 0 && (
+        <Link to={thanksPath}>
+          {thanksUrl}
+        </Link>
+      )}
+    </p>
   );
 }
 
