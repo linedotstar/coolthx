bedff3354157ae475a0c5f1d85e0e40bf2aeac34
linedotstar <linedotstar@gmail.com>
2021-04-10 17:44:51 -0700

    update the site title

--- src/Thanks/index.js
+++ src/Thanks/index.js
@@ -1,8 +1,13 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './Thanks.scss';
 
 function Thanks(props) {
   const { recipient } = useParams();
+
+  useEffect(() => {
+    document.title = `Cool Thx, ${recipient}`;
+  });
 
   return <h1 className='thanks'>Cool Thx, {recipient}</h1>;
 }
